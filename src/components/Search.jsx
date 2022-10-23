2250e825678d1a39e39be25a6479de60d7225f2a
Mick Maratta <mickmaratta@gmail.com>
2022-10-23 16:02:06 -0400

    Updated Search function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+  doc,
+} from "firebase/firestore";
 import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState();
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
@@ -27,6 +40,43 @@ const Search = () => {
     e.code === "Enter" && handleSearch();
   };
 
+  const handleSelect = async () => {
+    //Check whether this chat exists, if not create new one
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photUrl: user.photURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photUrl: currentUser.photURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      console.log(err);
+    }
+
+    //create user chats
+  };
+
   return (
     <div>
       <div className="p-2 relative">
@@ -37,18 +87,23 @@ const Search = () => {
           onKeyDown={handleKey}
           onChange={(e) => setUsername(e.target.value)}
         />
-      {username && <span className="text-slate-400 italic text-xs absolute top-2 right-2">Press Enter</span>}
+        {username && (
+          <span className="text-slate-400 italic text-xs absolute top-2 right-2">
+            Press Enter
+          </span>
+        )}
       </div>
-      {user && <div className="flex items-center space-x-2 p-2 border-solid border-b-2 border-gray-500 cursor-pointer hover:bg-slate-800 transition-all">
-        <img
-          className="w-8 h-8 rounded-full"
-          src={user.photoURL}
-          alt=""
-        />
-        <span className="text-white font-bold text-xs sm:text-sm md:text-base">
-          {user.displayName}
-        </span>
-      </div>}
+      {user && (
+        <div
+          onClick={handleSelect}
+          className="flex items-center space-x-2 p-2 border-solid border-b-2 border-gray-500 cursor-pointer hover:bg-slate-800 transition-all"
+        >
+          <img className="w-8 h-8 rounded-full" src={user.photoURL} alt="" />
+          <span className="text-white font-bold text-xs sm:text-sm md:text-base">
+            {user.displayName}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
